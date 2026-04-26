3efbf6c308fac771aaa8367bd7607a1cc9c119f3
Nagellan <ireknazmievirek@gmail.com>
2026-04-26 15:17:59 +0400

    Complete Exercise 5.1

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,4 +1,4 @@
-import React, { useReducer, useMemo, createContext, useContext } from 'react';
+import React, { useReducer, useMemo, useEffect, createContext, useContext } from 'react';
 
 import ToastShelf from '../ToastShelf';
 
@@ -11,10 +11,26 @@ function ToastProvider({ children }) {
         return [...state, action.payload];
       case 'remove':
         return state.filter((toast) => toast.id !== action.payload.id);
+      case 'reset':
+        return [];
       default:
         throw new Error('Unknown action type: ' + action.type);
     }
   }, []);
+
+  useEffect(() => {
+    const onKeyDown = (event) => {
+      if (event.code === 'Escape') {
+        dispatch({ type: 'reset' });
+      }
+    }
+
+    window.addEventListener('keydown', onKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', onKeyDown);
+    }
+  }, [dispatch])
 
   const value = useMemo(() => ({
     add: (payload) => dispatch({ type: 'add', payload }),
